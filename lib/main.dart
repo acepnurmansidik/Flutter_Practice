d54bf5bc70c0428e2332ff4deb9c9ea23c237296
acepnurmansidik <acepnurmansidik@gmail.com>
2022-01-27 10:00:35 +0700

    Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Says"),),
-        body: Center(child: Text("Hello World")),
+        appBar: AppBar(
+          title: const Text("Says"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.red.shade200,
+                width: 200,
+                height: 50,
+                child: const Text(
+                  "Investasikan uang anda untuk masa depan bersama kami",
+                  maxLines: 2, //maximal akan 2 baris
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                      fontSize: 20),
+                  overflow: TextOverflow
+                      .ellipsis, //akan memunculkan titik2 jika terlalu panjang
+                ))),
       ),
     );
   }
