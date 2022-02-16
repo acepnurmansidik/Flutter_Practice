63329013c3aa47081e2488384c668ab05f4d463b
acepnurmansidik <acepnurmansidik@gmail.com>
2022-02-16 09:19:31 +0700

    Stack & Align widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,261 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Random random = Random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: const Text("latihan text style"),
+            title: const Text("Stack & Align widget"),
           ),
-          body: Column(
+          body: Stack(
             children: <Widget>[
-              Flexible(
-                  flex: 1,
-                  child: Row(
+              // Stack 1 / background
+              Column(
+                children: <Widget>[
+                  Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: <Widget>[
+                          Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.amber[200],
+                              )),
+                          Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.blue[200],
+                              ))
+                        ],
+                      )),
+                  Flexible(
+                      flex: 1,
+                      child: Row(
+                        children: <Widget>[
+                          Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.blueGrey[200],
+                            ),
+                          ),
+                          Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.deepOrange[200],
+                              ))
+                        ],
+                      ))
+                ],
+              ),
+              ListView(
+                children: <Widget>[
+                  Column(
                     children: <Widget>[
-                      Flexible(
-                          flex: 1,
-                          child: Container(
-                            color: Colors.amberAccent[200],
-                          )),
-                      Flexible(
-                          flex: 1,
-                          child: Container(
-                            color: Colors.blueAccent[200],
-                          )),
-                      Flexible(
-                          flex: 1,
-                          child: Container(color: Colors.blueGrey[200]))
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        child: const Text(
+                          "This is middle stack",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ),
                     ],
-                  )),
-              Flexible(
-                  flex: 6,
-                  child: Container(
-                    color: Colors.blue[200],
-                  )),
-              Flexible(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.green[200],
-                  )),
+                  )
+                ],
+              ),
+              Align(
+                alignment: const Alignment(0, 1.015),
+                child: ElevatedButton(
+                  child: const Text("Button"),
+                  onPressed: () {},
+                ),
+              )
             ],
           )),
     );
